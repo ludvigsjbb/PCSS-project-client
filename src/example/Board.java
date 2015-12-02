4c4950e353d5aef730390cf1f3971ac3b810c982
ludvigsjbb <ludvigsjbb@gmail.com>
2015-12-02 13:58:04 +0100

    Created empty class with empty methods.

---
+++ src/example/Board.java
@@ -0,0 +1,27 @@
+
+public class Board {
+
+	
+	public Board(){}
+	
+	public void playerstatuss(){
+		
+		
+	}
+	
+	public void gameboard(){
+		
+			
+		
+	}
+	
+	public void mycards(){
+		
+	}
+	
+	public void displayplacedcards(){
+		
+	}
+	
+	
+}
