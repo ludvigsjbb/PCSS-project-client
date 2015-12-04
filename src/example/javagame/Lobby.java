95f95ec7f59df23f1be04c0cc4b63e9d017fd2d4
ludvigsjbb <ludvigsjbb@gmail.com>
2015-12-04 11:57:48 +0100

    implemented lobby button.

--- src/example/javagame/Lobby.java
+++ src/example/javagame/Lobby.java
@@ -15,8 +15,15 @@ public class Lobby extends BasicGameState {
 //	boolean host = false;
 	Image StartGame;
 	Image lobbyfull;
+	Image closed;
+	Image open;
+	
+	public boolean checkifopen;
+	
 	public Lobby (int state) {
 		
+		
+		checkifopen = true;
 		
 		//arraylist
 		String p1 = "playerone";
@@ -30,6 +37,8 @@ public class Lobby extends BasicGameState {
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		StartGame = new Image("res/StartGame.png");
 		lobbyfull = new Image("res/lobbyfull.png");
+		closed = new Image("res/closed.png");
+		open = new Image("res/open.png");
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
@@ -37,6 +46,12 @@ public class Lobby extends BasicGameState {
 		if (Menu.menuHost == true){  //not working
 			StartGame.draw(400, 510);
 			}
+		if(checkifopen == false){
+			closed.draw(620, 510);
+		}else{
+			open.draw(620, 510);
+		}
+		
 			//if (players.get(4) != null){ //ERRRRRROOOOOR HERE!!
 				//lobbyfull.draw(450, 510);
 			//}
@@ -49,8 +64,20 @@ public class Lobby extends BasicGameState {
 		int posX = Mouse.getX();
 		int posY = Mouse.getY();
 
-		// Start
+		//Open close lobby button
+		if(Menu.menuHost == true){
+			if ((posX > 620 && posX < 820) && (posY > 150 && posY < 190)) {
+				if (Mouse.isButtonDown(0) && checkifopen == true ) {
+					
+					checkifopen = false;
+					
+				}
+			}
+			}
 
+		
+		
+		// Start
 			if(Menu.menuHost == true){
 			if ((posX > 400 && posX < 600) && (posY > 150 && posY < 190)) {
 				if (Mouse.isButtonDown(0)) {
